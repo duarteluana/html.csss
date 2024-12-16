0f983057141294d494dd176421197efe5c50f63c
duarteluana <luanaduartesp@hotmail.com>
2024-12-16 09:53:05 -0300

    exercicios

---
+++ recode/javascript/oeradores.js
@@ -0,0 +1,25 @@
+//let escolha = 23;
+
+//console.log (escolha >= 10);
+//console.log (escolha <= 20);
+
+//console.log (escolha >= 10 && escolha <= 20); // exibe false no console
+
+//let escolha = 17;
+
+//console.log (escolha >= 10 && escolha <= 20); // exibe true no console
+
+//console.log (escolha >= 10 || escolha <= 20); // or ou um ou outro.
+
+//operador not !
+
+//quero ver o dom o tamanho da janela
+
+//console.log(window.innerWidth);
+
+window.onresize = function() {
+    console.log (window.innerWidth);
+}
+
+document.write("sera que escreve assim?")
+
---
+++ recode/javascript/script.js
@@ -0,0 +1,11 @@
+
+//let nome = prompt ("Qual é o seu nome?");
+    //document.write(nome + ", seja bem vinda ao site!");
+
+let opcao= confirm ("Deseja encerrar?");
+document.write("Sua escolha foi: " + opcao);
+
+function exibeMensagem() {
+    document.write("eu nao sei exatamente a capital do togo");
+
+}
